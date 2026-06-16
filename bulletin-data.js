ea83f8126c75f1e9227c18a7053b721357c971c9
Lim Shao Bao <limshaobao@gmail.com>
2026-06-16 09:51:48 +0900

    Update bulletin-data.js

--- bulletin-data.js
+++ bulletin-data.js
@@ -1,12 +1,23 @@
 const bulletins = [
+
+  {
+    date: "15 August 2026",
+    title: "Episode 5 Update",
+    content: [
+      "Episode 5 is underway in the UK! We highlight this month the extremely tight race for top spot in Card Goblin and Dark Sphere, which will no doubt only be settled in the upcoming final Episode 6. Good luck to all players as always!",
+
+      "Also, this weekend, WSEC is hosting its first ever Bushiroad Card Fight Championship in Milan, Italy. Good luck to all our UK reps!",
+
+      "Admin note: Huz Gaming results are now fully updated."
+    ]
+  },
+
   {
     date: "16 June 2026",
     title: "WSEC UK League Website Launched",
-    text: "Global standings, shop pages, player profiles and league statistics are now live."
-  },
-  {
-    date: "20 June 2026",
-    title: "Episode 1 Results Uploaded",
-    text: "The first episode standings have been uploaded and qualification positions updated."
+    content: [
+      "Global standings, shop pages, player profiles, and league statistics are now live."
+    ]
   }
+
 ];
