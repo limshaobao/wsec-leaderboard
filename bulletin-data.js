2800f5868a483705e6007d0ef18c7dde4a23832f
Lim Shao Bao <limshaobao@gmail.com>
2026-06-16 09:46:46 +0900

    Create bulletin-data.js

---
+++ bulletin-data.js
@@ -0,0 +1,12 @@
+const bulletins = [
+  {
+    date: "16 June 2026",
+    title: "WSEC UK League Website Launched",
+    text: "Global standings, shop pages, player profiles and league statistics are now live."
+  },
+  {
+    date: "20 June 2026",
+    title: "Episode 1 Results Uploaded",
+    text: "The first episode standings have been uploaded and qualification positions updated."
+  }
+];
