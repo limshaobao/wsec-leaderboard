ff1bcbc4ea56569c784b9a788e7519d69fe226c6
Lim Shao Bao <limshaobao@gmail.com>
2026-06-29 07:40:42 +0900

    Update bulletin-data.js

--- bulletin-data.js
+++ bulletin-data.js
@@ -6,9 +6,7 @@ window.bulletins = [
     content: [
       "Good evening to all UK players, we are pleased to announce we have confirmed how the extra 6 invites we receive this year due to total player numbers, will be distributed.",
 
-      "As per WSEC rules, our main source of invites to the WSEC Finals in Spain will be the 12 shop leaders (denoted in green), followed by 1-4 extra invites from the UK Open. We will also be rewarding ranking on the UK National Ladder; this year the top 2 players in the UK (denoted in gold) will receive automatic qualifications. The remaining 4 invites will be distributed in the Top 24 tournament (entry to this denoted by players in blue), for the top 4 players in that event.",
-
-      "Hopefully this clears up any confusion regarding the qualifications this year as we move into the final Episode - happy hunting!"
+      "As per WSEC rules, our main source of invites to the WSEC Finals in Spain will be the 12 shop leaders (denoted in green), followed by 1-4 extra invites from the UK Open. We will also be rewarding ranking on the UK National Ladder; this year the top 2 players in the UK (denoted in gold) will receive automatic qualifications. The remaining 4 invites will be distributed in the Top 24 tournament (entry to this denoted by players in blue), for the top 4 players in that event. Hopefully this clears up any confusion regarding the qualifications this year as we move into the final Episode - happy hunting!"
     ]
   },
   
